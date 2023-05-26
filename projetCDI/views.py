689543862f265a38b5aa4391b35b98ff3c51ccb5
plr <paul.thomas.stras@gmail.com>
2023-05-26 15:15:08 +0200

    Commit projet

--- projetCDI/views.py
+++ projetCDI/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse
-
+from django.shortcuts import render, redirect
 from .models import Livre
 
 
